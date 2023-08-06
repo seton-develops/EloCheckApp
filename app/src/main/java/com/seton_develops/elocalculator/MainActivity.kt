db521120b509f1859ddfc069932e263cdac9a95a
Sean Tonthat <sttt2017@gmail.com>
2023-08-06 03:35:51 -0700

    --updated layout to include spinner
    TODO:
    1) replace checkboxes with radio buttons
    2) add edit text in case user wants to add custom value
    3) extend Activity.OnItemSelectedListener so we can use spinner
    4) alert dialog if user selects win/lose/draw button to show how score is updated

--- app/src/main/java/com/seton_develops/elocalculator/MainActivity.kt
+++ app/src/main/java/com/seton_develops/elocalculator/MainActivity.kt
@@ -2,10 +2,24 @@ package com.seton_develops.elocalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spinnerKCoefficients: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        spinnerKCoefficients = findViewById(R.id.spinnerKCoefficients)
+        var spinnerAdapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            this,
+            R.array.FIDE_K_array,
+            android.R.layout.simple_spinner_item
+        )
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerKCoefficients.adapter = spinnerAdapter
     }
 }

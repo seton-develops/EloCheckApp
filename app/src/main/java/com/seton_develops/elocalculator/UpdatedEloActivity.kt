17ee076eb89f088e0a8fe4c60907bd519ce5a8dd
Sean Tonthat <sttt2017@gmail.com>
2023-08-08 01:43:30 -0700

    --added alert dialog for displaying updated elo scores --not finished and will require further revisions

---
+++ app/src/main/java/com/seton_develops/elocalculator/UpdatedEloActivity.kt
@@ -0,0 +1,11 @@
+package com.seton_develops.elocalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdatedEloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_updated_elo)
+    }
+}
